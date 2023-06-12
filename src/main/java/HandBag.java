eecd0c576a516aea055bd2b2c2219233839923c8
Inferfire <30782733+Inferfire@users.noreply.github.com>
2023-06-11 21:35:10 -0400

    Final HandBag.java
    Completed Handbag.java for exercise 3, with annotations.

--- src/main/java/HandBag.java
+++ src/main/java/HandBag.java
@@ -1,38 +1,19 @@
-/**
- * This file defines an child class named HandBag.
- * We have provided most of the code for this class, however,
- * you should implement the provided TODOs.
- *
- * You may find the readings in chapter 3. Relationships between Classes
- * helpful while working through this exercise.
- */
+// This file defines a child class named HandBag.
 public class HandBag extends Bag{
 
-    /**
-     * Creates a new HandBag with the given color and
+    /** Creates a new HandBag with the given color and
      * capacity.
      *
-     * @param color
-     * @param capacity
+     * @param color colour for the bag
+     * @param capacity capacity for the bag
      */
     public HandBag(String color, int capacity) {
-        /**
-         * This is how we call the parent's constructor
-         * The Python equivalent is super().__init__(...)
-         */
         super(color, capacity);
     }
-
-    /**
-     * Increase the capacity of this bag by 1.
-     */
+    
+    // Increase the capacity of this bag by 1.
     @Override
     public void enhance() {
-        /* TODO: Implement this method.
-         *       You may want to use the increaseCapacity() method that was
-         *       implemented in Bag.
-         *
-         * To call a method defined in a parent, you use super.method_name(...)
-         */
+        super.increaseCapacity(1);
     }
 }
